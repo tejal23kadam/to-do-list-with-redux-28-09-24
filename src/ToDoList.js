161752de489aff4e30d87e08cbd0bc5264e413c4
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-10-04 10:12:51 +0530

    commitfrrr

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,28 +1,98 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addTodo,updateTodo,deleteTodo,deleteAllTodo } from "./todoSlice";
+import { addTodo, updateTodo, deleteTodo, deleteAllTodo, toggleComplete } from "./todoSlice";
+import './ToDoCss.css';
 
 const ToDoList = () => {
   const [text, setText] = useState("");
-  
+  const [selectedId, setSelectedID] = useState(-1);
+  const [idEdit, setIdEdit] = useState(null);
+  const [completedToDo, setCompletedToDo] = useState('All');
+  const [editing, setEditing] = useState(false);
+  const [count, setCount] = useState(0);
+  let editMode = {};
+
+  const setEditMode = () => {
+    if (editing) {
+      editMode.display = 'none';
+      setIdEdit(null);
+    }
+    else {
+      editMode.display = 'block';
+      setIdEdit(null);
+    }
+  }
+
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <input type="text" value={text} onChange={e => { setText(e.target.value); }} />
-            <button onClick={() => { dispatch(addTodo(text)); setText(""); }} >Add</button>
-            <button onClick={() => { dispatch(deleteAllTodo()) }}> Delete All </button>
-            
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id}>
-                        {todo.text}                                
-                        <button onClick={(e) => {dispatch(updateTodo({ id:todo.id, text:e.target.value})); setText(todo.text); }}> update</button>
-                        <button onClick={() => { dispatch(deleteTodo(todo.id)); }}> Delete </button>
-                    </li>
-                ))}
-            </ul>
+    <div className='main'>
+      <div>
+        <input type="text" value={text} onChange={e => { setText(e.target.value); }} />
+        <button onClick={() => { dispatch(addTodo(text)); setText(""); }} >Add</button>
+        <button onClick={() => { dispatch(deleteAllTodo()) }}> Delete All </button>
+      </div>
+      <div >
+        <button onClick={() => setCompletedToDo(null)} >All</button>
+        <button onClick={() => setCompletedToDo(true)}> Completed </button>
+        <button onClick={() => setCompletedToDo(false)}> InComplete </button>
+
+      </div>
+
+      <ul>
+
+        {(todos && todos.length) ? (
+          // todos.filter(item => item.completed === 'All').map((item, idx) =>
+            todos.map((item, idx) =>
+            <li key={item.id} style={{ textDecoration: item.completed ? "line-through" : "none" }}>
+              {
+                (selectedId === idx) ? (
+                  <div>
+                    <div style={{ display: idEdit === item.id ? 'block' : 'none' }}>
+                      <input type='text' value={item.text} onChange={(e) => dispatch(updateTodo({ id: item.id, text: e.target.value }))} />
+                    </div>
+                    <div className='updated-listing' >
+                      <div>
+                        <p style={{ display: idEdit === item.id ? 'none' : 'block' }}>{item.text}</p>
+                      </div>
+                      <div>
+                        {
+                          (editing) ? (
+                            <button onClick={() => { setEditMode(); setEditing(false) }}>save</button>
+                          )
+                            :
+                            (
+                              <button onClick={() => { setSelectedID(idx); setIdEdit(item.id); setEditing(true) }}
+                                onChange={(e) => dispatch(updateTodo({ id: item.id, text: e.target.value }))}>edit</button>
+                            )
+                        }
+                        <button onClick={() => { dispatch(deleteTodo(item.id));; setCount(count - 1); }}>Delete</button>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div style={{ editing }} className='normal-listing'>
+                    <div >
+                      <p>{item.text}</p>
+
+                    </div>
+                    <div>
+                      <button onClick={() => dispatch(toggleComplete(item.id))}>
+                        {item.completed ? "Mark Incomplete" : "Mark Complete"}{" "}
+                      </button>
+                      <button onClick={() => { setSelectedID(idx); setIdEdit(item.id); setEditing(true) }}
+                        onChange={(e) => dispatch(updateTodo({ id: item.id, text: e.target.value }))}>edit</button>
+
+                      <button onClick={() => { dispatch(deleteTodo(item.id)); setCount(count - 1); }}>Delete</button>
+                    </div>
+                  </div>
+                )
+              }
+            </li>
+          )) : <h1>"No todos, yay!"</h1>
+        }
+      </ul >
     </div>
   );
 };
--- src/todoSlice.js
+++ src/todoSlice.js
@@ -12,24 +12,28 @@ const todoSlice = createSlice({
       };
       state.push(newTodo);
     },
-
+    toggleComplete: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
     updateTodo: (state, action) => {
-      const index = state.findIndex(item => item.id === action.payload.id);
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
       const updatedState = [...state];
       updatedState[index].text = action.payload.text;
     },
-
+   
     deleteTodo: (state, action) => {
       const index = state.findIndex((todo) => todo.id === action.payload);
       if (index !== -1) {
         state.splice(index, 1);
       }
     },
-    
     deleteAllTodo: (state, action) => {
-      state.splice(0);
+      state.splice(0); //if you don't specify the end point of an array it will delete all the array starting from the given position.
     },
   },
 });
-export const { addTodo, updateTodo, deleteTodo, deleteAllTodo } = todoSlice.actions;
+export const { addTodo, updateTodo, deleteTodo, deleteAllTodo, toggleComplete} = todoSlice.actions;
 export default todoSlice.reducer;
